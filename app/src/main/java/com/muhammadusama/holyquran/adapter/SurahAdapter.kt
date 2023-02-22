2370c3418358e66297a00f96870e586725ab405f
imusama <usama.muhammad@techlogix.com>
2023-02-22 15:34:01 +0500

    Data Binding Implemented

--- app/src/main/java/com/muhammadusama/holyquran/adapter/SurahAdapter.kt
+++ app/src/main/java/com/muhammadusama/holyquran/adapter/SurahAdapter.kt
@@ -7,30 +7,54 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.muhammadusama.holyquran.R
+import com.muhammadusama.holyquran.databinding.SurahItemLayoutBinding
 import com.muhammadusama.holyquran.models.Data
 
 class SurahAdapter(val itemsList: List<Data>,val context:Context):
     RecyclerView.Adapter<SurahAdapter.SurahViewHolder>() {
 
-    class SurahViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-        val surahNumber = itemView.findViewById<TextView>(R.id.textView5)
-        val surahNameEnglish = itemView.findViewById<TextView>(R.id.textView)
-        val surahRevelation = itemView.findViewById<TextView>(R.id.textView2)
-        val surahTotalAyah = itemView.findViewById<TextView>(R.id.textView3)
-        val surahNameArabic = itemView.findViewById<TextView>(R.id.textView4)
+    private lateinit var binding: SurahItemLayoutBinding
+
+        // For simple implementation
+//    class SurahViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+//        val surahNumber = itemView.findViewById<TextView>(R.id.surahNumber)
+//        val surahNameEnglish = itemView.findViewById<TextView>(R.id.surahNameEnglish)
+//        val surahRevelation = itemView.findViewById<TextView>(R.id.surahRevelation)
+//        val surahTotalAyah = itemView.findViewById<TextView>(R.id.surahTotalAyah)
+//        val surahNameArabic = itemView.findViewById<TextView>(R.id.surahNameArabic)
+//    }
+
+    // With Data Binding
+    class SurahViewHolder(
+        private val binding: SurahItemLayoutBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(surahData: Data) {
+            binding.surahItem = surahData
+        }
     }
 
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SurahViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.surah_item_layout,parent,false)
-        return SurahViewHolder(view);
+        // For simple implementation
+//        val view = LayoutInflater.from(parent.context).inflate(R.layout.surah_item_layout,parent,false)
+//        return SurahViewHolder(view);
+        // With Data Binding
+        binding = SurahItemLayoutBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return SurahViewHolder(binding)
+
     }
 
     override fun onBindViewHolder(holder: SurahViewHolder, position: Int) {
-        holder.surahNumber.text = itemsList[position].number.toString()
-        holder.surahNameEnglish.text = itemsList[position].englishName.toString()
-        holder.surahRevelation.text = itemsList[position].revelationType.toString()
-        holder.surahTotalAyah.text = itemsList[position].numberOfAyahs.toString()+ " " + "Verses"
-        holder.surahNameArabic.text = itemsList[position].name.toString()
+        //For Simple Implementation
+//        holder.surahNumber.text = itemsList[position].number.toString()
+//        holder.surahNameEnglish.text = itemsList[position].englishName.toString()
+//        holder.surahRevelation.text = itemsList[position].revelationType.toString()
+//        holder.surahTotalAyah.text = itemsList[position].numberOfAyahs.toString()+ " " + "Verses"
+//        holder.surahNameArabic.text = itemsList[position].name.toString()
+
+        // With Data Binding
+        val surahList = itemsList[position]
+        holder.bind(surahList)
     }
 
     override fun getItemCount(): Int {
